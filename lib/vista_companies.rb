73dc34ed38fc902cd40eb0515a158b4529573220
Ian Gutwinski <iangutwinski@gmail.com>
2019-04-06 15:58:40 +0000

    first commit

---
+++ lib/vista_companies.rb
@@ -0,0 +1,6 @@
+require "vista_companies/version"
+
+module VistaCompanies
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/vista_companies/version.rb
@@ -0,0 +1,3 @@
+module VistaCompanies
+  VERSION = "0.1.0"
+end
