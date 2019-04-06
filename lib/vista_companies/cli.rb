751194c5eda10ca8a002caedac133b78aadd388f
Ian Gutwinski <iangutwinski@gmail.com>
2019-04-06 17:26:16 +0000

    added to call method

--- lib/vista_companies/cli.rb
+++ lib/vista_companies/cli.rb
@@ -1,6 +1,26 @@
 #CLI Controller
 class VistaCompanies::CLI
   def call
-    puts "Vista's Portfolio Companies:"
+    puts "Welcome to the private equity fund portfolio company scraper."
+    puts "This program allows users to navigate Vista Equity Partners' current and former investments."
+    puts "You can navigate the program in the following ways:"
+    puts "1. Enter '1' to list all current portfolio companies alphabetically."
+    puts "2. Enter '2' to list all former portfolio companies alphabetically."
+    puts "3. Enter '3' to list all portfolio companies alphabetically (whether current or former)."
+    puts "4. Enter '4' to select companies by industry classification."
   end
+
 end
+
+#Welcome user. Explain the program.
+#Offer 1 for current, 2 for former, or 3 for all, alphabetically, or 4 for by industry
+
+#If 1, 2 or 3
+#List out company names with a number
+#then list out details of an individual company if a user inputs the number
+#Some further prompt for "more detail"
+
+#If 4,
+#List out all the industry categorizations with numbers
+#prompt user for input of a number
+#then list out all companies in that industry
