593b7aec9c4e44ec11d632fa3efa88bb7272fd5c
Ian Gutwinski <iangutwinski@gmail.com>
2019-04-07 22:01:44 +0000

    added scraper functionality

--- lib/vista_companies/scraper.rb
+++ lib/vista_companies/scraper.rb
@@ -2,4 +2,12 @@ require 'open-uri'
 
 class Scraper
 
+  def self.scrape_company_page(index_url) #https://www.vistaequitypartners.com/companies/
+    company_page = Nokogiri::HTML(open(index_url))
+    companies = []
+    company_page.css("div.all-companies grid3")
+
+  end
+
+
 end
