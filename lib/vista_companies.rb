50388d50b0382273fa1979f99af5836083e7d19e
Ian Gutwinski <iangutwinski@gmail.com>
2019-04-07 22:27:56 +0000

    added portco and scraper to environment

--- lib/vista_companies.rb
+++ lib/vista_companies.rb
@@ -1,2 +1,4 @@
 require_relative "./vista_companies/version"
 require_relative "./vista_companies/cli.rb"
+require_relative "./vista_companies/portco.rb"
+require_relative "./vista_companies/scraper.rb"
--- lib/vista_companies/scraper.rb
+++ lib/vista_companies/scraper.rb
@@ -2,10 +2,24 @@ require 'open-uri'
 
 class Scraper
 
-  def self.scrape_company_page(index_url) #https://www.vistaequitypartners.com/companies/
-    company_page = Nokogiri::HTML(open(index_url))
+  def self.scrape_all_companies(index_url) #https://www.vistaequitypartners.com/companies/
+    company_listing = Nokogiri::HTML(open(index_url))
     companies = []
-    company_page.css("div.all-companies grid3")
+    company_listing.css("div.all-companies grid3").each do |company|
+      company.css("a.grid-item").each do |link|
+        company_site = "#{link.attr('href')}"
+      end
+    end
+  end
+
+  def self.scrape_company_page(company_slug)
+    company = {}
+    company_page = Nokogiri::HTML(open(company_slug))
+    details = company_page.css("div.details") #need to go through and find Year of Investment, then take following text, etc.
+    company[:year_of_investment] = x
+    company[:portfolio_status] = x
+    company[:headquarters] = x
+    company[:company_site] = company_page.css("div.details a").attr('href')
 
   end
 
