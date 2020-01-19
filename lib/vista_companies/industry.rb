7d3dc4bd1f1c91e22b20ee97b0e7b71ee29b4cc3
Ian Gutwinski <iangutwinski@gmail.com>
2020-01-19 03:52:32 +0000

    updated industry class

--- lib/vista_companies/industry.rb
+++ lib/vista_companies/industry.rb
@@ -1,9 +1,10 @@
 class Industry
 attr_accessor :ind_name
+attr_reader :portcos
 
 @@all = []
 
-  def initalize(ind_name)
+  def initialize(ind_name)
     @ind_name = ind_name
     @portcos = []
   end
@@ -14,14 +15,6 @@ attr_accessor :ind_name
 
   def self.destroy_all
     all.clear
-  end
-
-  def save
-      self.class.all << self
-  end
-
-  def self.save(ind_name)
-    new.ind_name.tap( |x| x.save )
   end
 
 end
